153d84a9146e9cd725950f642a2d9070349dfecd
sekelsta <sekelsta@protonmail.com>
2022-11-13 08:28:32 -0500

    Update for Minecraft 1.19.2

--- src/main/java/horse_tack/HorseTack.java
+++ src/main/java/horse_tack/HorseTack.java
@@ -1,14 +1,9 @@
 package sekelsta.horse_tack;
 
 import net.minecraftforge.fml.common.Mod;
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
 
 @Mod(HorseTack.MODID)
 public class HorseTack
 {
     public static final String MODID = "horse_tack";
-
-    // Directly reference a log4j logger.
-    //private static final Logger LOGGER = LogManager.getLogger();
 }
