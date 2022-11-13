bc366c11f192a18a10be37bbd486e40fd6e4d256
sekelsta <sekelsta@protonmail.com>
2022-11-13 10:39:55 -0500

    Fix issue where items were not being registered, also fix some registries

--- src/main/java/horse_tack/HorseTack.java
+++ src/main/java/horse_tack/HorseTack.java
@@ -1,9 +1,14 @@
 package sekelsta.horse_tack;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(HorseTack.MODID)
 public class HorseTack
 {
     public static final String MODID = "horse_tack";
+
+    public HorseTack() {
+        ModItems.ITEM_DEFERRED.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
 }
